b45efc8071d55f19cd625f031b2069f6b1910174
Luiz Gabriel Pessanha <144502476+Ziulzin251@users.noreply.github.com>
2024-06-01 18:34:33 -0300

    Function fundovem

--- Js/fundos_metro.js
+++ Js/fundos_metro.js
@@ -190,59 +190,147 @@ function FundoVem(){
     switch(x){
         case 0:
             //Barra de Navegação
-            Nav.classList.remove('messenger', 'ori')
+            Nav.classList.remove('messenger', 'ori', 'rain', 'blood', 'blas')
             Nav.classList.add('hollow')
 
             //Listas Laterais
-            ItemLateral.classList.remove('messenger-lateral', 'ori-lateral')
-            ItemLateral2.classList.remove('messenger-lateral', 'ori-lateral')
-            ItemLateral3.classList.remove('messenger-lateral', 'ori-lateral')
+            ItemLateral.classList.remove('messenger-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral2.classList.remove('messenger-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral3.classList.remove('messenger-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral4.classList.remove('messenger-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral5.classList.remove('messenger-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral6.classList.remove('messenger-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
 
             ItemLateral.classList.add('hollow-lateral')
             ItemLateral2.classList.add('hollow-lateral')
             ItemLateral3.classList.add('hollow-lateral')
+            ItemLateral4.classList.add('hollow-lateral')
+            ItemLateral5.classList.add('hollow-lateral')
+            ItemLateral6.classList.add('hollow-lateral')
 
             //Rodape
-            Rodape.classList.remove('messenger', 'ori')
+            Rodape.classList.remove('messenger', 'ori', 'rain', 'blood', 'blas')
             Rodape.classList.add('hollow')
             break
         case 1:
             //Barra de Navegação
-            Nav.classList.remove('hollow', 'ori')
+            Nav.classList.remove('hollow', 'ori', 'rain', 'blood', 'blas')
             Nav.classList.add('messenger')
 
             //Listas Laterais
-            ItemLateral.classList.remove('hollow-lateral', 'ori-lateral')
-            ItemLateral2.classList.remove('hollow-lateral', 'ori-lateral')
-            ItemLateral3.classList.remove('hollow-lateral', 'ori-lateral')
+            ItemLateral.classList.remove('hollow-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral2.classList.remove('hollow-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral3.classList.remove('hollow-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral4.classList.remove('hollow-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral5.classList.remove('hollow-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral6.classList.remove('hollow-lateral', 'ori-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
 
             ItemLateral.classList.add('messenger-lateral')
             ItemLateral2.classList.add('messenger-lateral')
             ItemLateral3.classList.add('messenger-lateral')
+            ItemLateral4.classList.add('messenger-lateral')
+            ItemLateral5.classList.add('messenger-lateral')
+            ItemLateral6.classList.add('messenger-lateral')
             
-
             //Rodape
-            Rodape.classList.remove('hollow', 'ori')
+            Rodape.classList.remove('hollow', 'ori', 'rain', 'blood', 'blas')
             Rodape.classList.add('messenger')
             break
         case 2:
             //Barra de Navegação
-            Nav.classList.remove('messenger', 'ori')
+            Nav.classList.remove('messenger', 'hollow', 'rain', 'blood', 'blas')
             Nav.classList.add('ori')
 
             //Listas Laterais
-            ItemLateral.classList.remove('messenger-lateral', 'hollow-lateral')
-            ItemLateral2.classList.remove('messenger-lateral', 'hollow-lateral')
-            ItemLateral3.classList.remove('messenger-lateral', 'hollow-lateral')
+            ItemLateral.classList.remove('messenger-lateral', 'hollow-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral2.classList.remove('messenger-lateral', 'hollow-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral3.classList.remove('messenger-lateral', 'hollow-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral4.classList.remove('messenger-lateral', 'hollow-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral5.classList.remove('messenger-lateral', 'hollow-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral6.classList.remove('messenger-lateral', 'hollow-lateral', 'rain-lateral', 'blood-lateral', 'blas-lateral')
 
             ItemLateral.classList.add('ori-lateral')
             ItemLateral2.classList.add('ori-lateral')
             ItemLateral3.classList.add('ori-lateral')
-            
+            ItemLateral4.classList.add('ori-lateral')
+            ItemLateral5.classList.add('ori-lateral')
+            ItemLateral6.classList.add('ori-lateral')
 
             //Rodape
-            Rodape.classList.remove('hollow', 'messenger')
+            Rodape.classList.remove('hollow', 'messenger', 'rain', 'blood', 'blas')
             Rodape.classList.add('ori')
+            break
+        case 3:
+            //Barra de Navegação
+            Nav.classList.remove('messenger', 'hollow', 'ori', 'blood', 'blas')
+            Nav.classList.add('rain')
+
+            //Listas Laterais
+            ItemLateral.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral2.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral3.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral4.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral5.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'blas-lateral')
+            ItemLateral6.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'blas-lateral')
+
+            ItemLateral.classList.add('rain-lateral')
+            ItemLateral2.classList.add('rain-lateral')
+            ItemLateral3.classList.add('rain-lateral')
+            ItemLateral4.classList.add('rain-lateral')
+            ItemLateral5.classList.add('rain-lateral')
+            ItemLateral6.classList.add('rain-lateral')
+
+            //Rodape
+            Rodape.classList.remove('hollow', 'messenger', 'ori', 'blood', 'blas')
+            Rodape.classList.add('rain')
+            break
+        case 4:
+            //Barra de Navegação
+            Nav.classList.remove('messenger', 'hollow', 'ori', 'blood', 'rain')
+            Nav.classList.add('blas')
+
+            //Listas Laterais
+            ItemLateral.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'rain-lateral')
+            ItemLateral2.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'rain-lateral')
+            ItemLateral3.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'rain-lateral')
+            ItemLateral4.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'rain-lateral')
+            ItemLateral5.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'rain-lateral')
+            ItemLateral6.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blood-lateral', 'rain-lateral')
+
+            ItemLateral.classList.add('blas-lateral')
+            ItemLateral2.classList.add('blas-lateral')
+            ItemLateral3.classList.add('blas-lateral')
+            ItemLateral4.classList.add('blas-lateral')
+            ItemLateral5.classList.add('blas-lateral')
+            ItemLateral6.classList.add('blas-lateral')
+
+            //Rodape
+            Rodape.classList.remove('hollow', 'messenger', 'ori', 'blood', 'rain')
+            Rodape.classList.add('blas')
+            break
+        case 5:
+            //Barra de Navegação
+            Nav.classList.remove('messenger', 'hollow', 'ori', 'blas', 'rain')
+            Nav.classList.add('blood')
+
+            //Listas Laterais
+            ItemLateral.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blas-lateral', 'rain-lateral')
+            ItemLateral2.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blas-lateral', 'rain-lateral')
+            ItemLateral3.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blas-lateral', 'rain-lateral')
+            ItemLateral4.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blas-lateral', 'rain-lateral')
+            ItemLateral5.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blas-lateral', 'rain-lateral')
+            ItemLateral6.classList.remove('messenger-lateral', 'hollow-lateral', 'ori-lateral', 'blas-lateral', 'rain-lateral')
+
+            ItemLateral.classList.add('blood-lateral')
+            ItemLateral2.classList.add('blood-lateral')
+            ItemLateral3.classList.add('blood-lateral')
+            ItemLateral4.classList.add('blood-lateral')
+            ItemLateral5.classList.add('blood-lateral')
+            ItemLateral6.classList.add('blood-lateral')
+
+            //Rodape
+            Rodape.classList.remove('hollow', 'messenger', 'ori', 'blas', 'rain')
+            Rodape.classList.add('blood')
             break
     }
 
